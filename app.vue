25b07501bd7ef866c4e60381754751363b44db56
mylo <milopadma@yahoo.com>
2024-06-20 16:09:35 +0800

    feat: template and fonts

--- app.vue
+++ app.vue
@@ -1,5 +1,59 @@
 <template>
   <div>
-    hello
+    <nav>
+      <div>Frankliné&Co.</div>
+      <div>Products, Solutions, Stories </div>
+      <div>Menu</div>
+    </nav>
+    <header>
+      <h1>Maréchal Verchetti</h1>
+    </header>
+    <section>
+      <img src="/chair.jpg" alt="chair" />
+      <p>
+        Maréchal Verchetti is built on a legacy of creative partnership. In a first-of-its-kind collaboration, the
+        Frankliné Jeremy design house thought fully reimagines eight ●
+      </p>
+      <hr class="border-t-2 border-gray-300 my-4" />
+      <p>
+        /THESE CHAIRS CAN BE
+      </p>
+      <p>
+        These chairs can be upholstered in just about any fabric you like. When
+        upholstered, the chair&rsquo;s back and seat have exposed veneer on the
+        back sides. Veneers are available in six wood.
+      </p>
+    </section>
+    <section>
+      horizontal scrolling images
+    </section>
+    <section>
+      <h2>Fine forms, — Refined.</h2>
+      <p>
+        /THESE CHAIRS CAN BE
+      </p>
+      <p>
+        Fresh eyes see new possibilities for classic Eames designs, including a bold new colour palette and updated
+        materials.
+      </p>
+    </section>
+    <section>
+      <img src="/threechairs.jpg" alt="three chairs">
+      <p>
+        Maréchal Verchetti is built on a legacy of creative partnership. In a first-of-its-kind collaboration, the
+        Frankliné Jeremy design house thought fully reimagines eight ●
+      </p>
+      <p>Fresh eyes see new possibilities for classic Eames designs, including a bold new colour palette and updated
+        materials.</p>
+    </section>
+    <section>
+      <h3> Herman Miller is built on a legacy of creative partnership. In a first-of-its-kind collaboration, the Danish
+        design house HAY</h3>
+      <img src="/graychairs.jpg" alt="gray chairs">
+      <img src="/graychair.jpg" alt="gray chair">
+    </section>
+    <footer>
+      <h2>Another Collective</h2>
+    </footer>
   </div>
 </template>
