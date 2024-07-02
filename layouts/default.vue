a899c6724a020e55958a12ced6c8c14735b960b3
mylo <milopadma@yahoo.com>
2024-07-02 15:03:41 +0800

    feat: nav to ts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,113 @@
+<script setup lang="ts">
+import { gsap } from "gsap";
+import CursorFollower from "../components/cursorfollower.vue";
+import { SpeedInsights } from "@vercel/speed-insights/vue";
+import SmoothScroll from "../components/smoothscroll.vue";
+import DebugToggle from "../components/debugtoggle.vue";
+import { store } from "../store";
+import { ref, onMounted, watch } from "vue";
+import { useRoute } from "vue-router";
+
+const selectedLink = ref<string | null>(null);
+const route = useRoute();
+
+const changeCursorToImage = () => {
+  const cursorFollower = ref<InstanceType<typeof CursorFollower> | null>(null);
+  if (cursorFollower.value) {
+    cursorFollower.value.changeCursorToImage();
+  } else {
+    console.log("cursorFollower ref is not defined");
+  }
+};
+
+const resetCursor = () => {
+  const cursorFollower = ref<InstanceType<typeof CursorFollower> | null>(null);
+  if (cursorFollower.value) {
+    cursorFollower.value.resetCursor();
+  } else {
+    console.log("cursorFollower ref is not defined");
+  }
+};
+
+const animateLinks = () => {
+  gsap.killTweensOf(document.querySelectorAll("#nav-links-text"));
+  const linkTexts = document.querySelectorAll("#nav-links-text");
+  gsap.to(linkTexts, { opacity: 1, duration: 0 });
+  linkTexts.forEach((linkText) => {
+    if (selectedLink.value === "") {
+      gsap.to(linkText, { opacity: 1, duration: 0.5 });
+    } else if (
+      linkText.textContent?.trim().toLowerCase() !== selectedLink.value
+    ) {
+      gsap.to(linkText, { opacity: 0.2, duration: 0.5 });
+    } else {
+      gsap.to(linkText, { opacity: 1, duration: 0.5 });
+    }
+  });
+
+  // animate circle fading in when it exists
+  const circles = document.querySelectorAll(".circle");
+  gsap.fromTo(circles, { opacity: 0 }, { opacity: 1, duration: 0.5 });
+};
+
+const animateScroll = () => {
+  store.scroll.on("scroll", (args: any) => {
+    const navElements = document.querySelectorAll("#main-nav");
+    if (args.scroll.y > 30) {
+      gsap.to(navElements, {
+        translateY: "-50%",
+        paddingLeft: "2rem",
+        paddingRight: "2rem",
+        duration: 0.3,
+        ease: "circ.out",
+      }); // add padding to nav elements
+    } else {
+      gsap.to(navElements, {
+        translateY: "0%",
+        paddingLeft: "0",
+        paddingRight: "0",
+        duration: 0.3,
+        ease: "circ.out",
+      }); // remove padding from nav elements
+    }
+  });
+};
+
+const checkCurrentUrl = () => {
+  const path = window.location.pathname;
+  if (path.includes("products")) {
+    selectedLink.value = "products,";
+    animateLinks();
+  } else if (path.includes("solutions")) {
+    selectedLink.value = "solutions,";
+    animateLinks();
+  } else if (path.includes("stories")) {
+    selectedLink.value = "stories";
+    animateLinks();
+  } else if (path.includes("")) {
+    selectedLink.value = "";
+    animateLinks();
+  } else {
+    selectedLink.value = null;
+  }
+};
+
+onMounted(() => {
+  checkCurrentUrl();
+});
+
+watch(route, () => {
+  checkCurrentUrl();
+  animateLinks();
+});
+</script>
+
 <template>
-  <SpeedInsights />
   <div>
+    <SpeedInsights />
     <nav
       id="main-nav"
-      class="col-span-9 grid grid-cols-9 font-[530] tracking-[-0.01em] relative bg-[hsla(0,0%,100%,0.4)] z-50 backdrop-blur-md pt-16 pb-4 mx-[60px]"
+      class="col-span-9 grid grid-cols-9 font-[530] tracking-[-0.01em] relative bg-[hsla(0,0%,100%,0.4)] z-50 backdrop-blur-md pt-16 pb-4 px-[60px]"
     >
       <NuxtLink to="/" id="nav-logo" class="cursor-pointer col-span-1"
         >Frankliné&Co.</NuxtLink
@@ -53,137 +157,6 @@
     <DebugToggle />
   </div>
 </template>
-
-<script>
-import { gsap } from "gsap";
-import CursorFollower from "~/components/cursorfollower.vue";
-import { SpeedInsights } from "@vercel/speed-insights/vue";
-import SmoothScroll from "~/components/smoothscroll.vue";
-import DebugToggle from "~/components/debugtoggle.vue";
-import { store } from "~/store";
-
-export default {
-  components: {
-    CursorFollower,
-    SpeedInsights,
-    SmoothScroll,
-    DebugToggle,
-  },
-  setup() {
-    return { store };
-  },
-  data() {
-    return {
-      selectedLink: null,
-    };
-  },
-  mounted() {
-    this.checkCurrentUrl();
-  },
-  watch: {
-    $route() {
-      this.checkCurrentUrl();
-      this.animateLinks();
-    },
-  },
-  methods: {
-    changeCursorToImage() {
-      if (this.$refs.cursorFollower) {
-        this.$refs.cursorFollower.changeCursorToImage();
-      } else {
-        console.log("cursorFollower ref is not defined");
-      }
-    },
-    resetCursor() {
-      if (this.$refs.cursorFollower) {
-        this.$refs.cursorFollower.resetCursor();
-      } else {
-        console.log("cursorFollower ref is not defined");
-      }
-    },
-
-    locomotiveScrollInit() {
-      store.scroll = new this.$LocomotiveScroll({
-        el: document.querySelector("[data-scroll-container]"),
-        smooth: true,
-        getDirection: true,
-        mobile: {
-          smooth: true,
-          getDirection: true,
-        },
-        tablet: {
-          smooth: true,
-          getDirection: true,
-          breakpoint: 0,
-        },
-      });
-      console.log("LocomotiveScroll initialized:", store.scroll);
-      this.animateScroll();
-    },
-
-    animateLinks() {
-      gsap.killTweensOf(document.querySelectorAll("#nav-links-text"));
-      const linkTexts = document.querySelectorAll("#nav-links-text");
-      gsap.to(linkTexts, { opacity: 1, duration: 0 });
-      linkTexts.forEach((linkText) => {
-        if (this.selectedLink === "") {
-          gsap.to(linkText, { opacity: 1, duration: 0.5 });
-        } else if (
-          linkText.textContent.trim().toLowerCase() !== this.selectedLink
-        ) {
-          gsap.to(linkText, { opacity: 0.2, duration: 0.5 });
-        } else {
-          gsap.to(linkText, { opacity: 1, duration: 0.5 });
-        }
-      });
-
-      // animate circle fading in when it exists
-      const circles = document.querySelectorAll(".circle");
-      gsap.fromTo(circles, { opacity: 0 }, { opacity: 1, duration: 0.5 });
-    },
-    animateScroll() {
-      store.scroll.on("scroll", (args) => {
-        const navElements = document.querySelectorAll("#main-nav");
-        if (args.scroll.y > 30) {
-          this.$gsap.to(navElements, {
-            translateY: "-50%",
-            paddingLeft: "2rem",
-            paddingRight: "2rem",
-            duration: 0.3,
-            ease: "circ.out",
-          }); // add padding to nav elements
-        } else {
-          this.$gsap.to(navElements, {
-            translateY: "0%",
-            paddingLeft: "0",
-            paddingRight: "0",
-            duration: 0.3,
-            ease: "circ.out",
-          }); // remove padding from nav elements
-        }
-      });
-    },
-    checkCurrentUrl() {
-      const path = window.location.pathname;
-      if (path.includes("products")) {
-        this.selectedLink = "products,";
-        this.animateLinks();
-      } else if (path.includes("solutions")) {
-        this.selectedLink = "solutions,";
-        this.animateLinks();
-      } else if (path.includes("stories")) {
-        this.selectedLink = "stories";
-        this.animateLinks();
-      } else if (path.includes("")) {
-        this.selectedLink = "";
-        this.animateLinks();
-      } else {
-        this.selectedLink = null;
-      }
-    },
-  },
-};
-</script>
 
 <style scoped>
 .circle {
